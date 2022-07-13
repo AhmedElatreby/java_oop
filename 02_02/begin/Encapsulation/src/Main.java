6ebcba5a9258f657e95c031577c27cae22a2d246
ahmedsport <ahmedsport@hotmail.com>
2022-07-13 21:17:35 +0100

    getter and setter

--- 02_02/begin/Encapsulation/src/Main.java
+++ 02_02/begin/Encapsulation/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
+        Tree myTree = new Tree(120, 50, TreeType.WALNUT);
+        //System.out.println(myTree.heightFt);
+
+        myTree.announceTallTree();
 
     }
 }
--- 02_03/start/Encapsulation/src/Tree.java
+++ 02_03/start/Encapsulation/src/Tree.java
@@ -15,6 +15,38 @@ public class Tree {
         this.treeType = treeType;
     }
 
+    public static Color getTrunkColor() {
+        return trunkColor;
+    }
+
+    public static void setTrunkColor(Color trunkColor) {
+        Tree.trunkColor = trunkColor;
+    }
+
+    public double getHeightFt() {
+        return heightFt;
+    }
+
+    public void setHeightFt(double heightFt) {
+        this.heightFt = heightFt;
+    }
+
+    public double getTrunkDiameterInches() {
+        return trunkDiameterInches;
+    }
+
+    public void setTrunkDiameterInches(double trunkDiameterInches) {
+        this.trunkDiameterInches = trunkDiameterInches;
+    }
+
+    public TreeType getTreeType() {
+        return treeType;
+    }
+
+    public void setTreeType(TreeType treeType) {
+        this.treeType = treeType;
+    }
+
     public void grow() {
         this.heightFt = heightFt + 10;
         this.trunkDiameterInches = trunkDiameterInches + 1;
